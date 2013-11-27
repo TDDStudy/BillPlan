096e4c14b2acd639a4c0db78dbf35e505aec0940
Seungwon Kim <closer527@gmail.com>
2013-11-27 22:42:14 +0900

    refactoring some methods

--- src/main/java/billingPack/Billing.java
+++ src/main/java/billingPack/Billing.java
@@ -12,25 +12,31 @@ public class Billing {
     public Billing() {}
     public Billing(Line line) {
         this.line = line;
+        decideRate();   // PlanType에 따라 요금 비율 정해줌.
     }
 
-    public double getExpectedSimpleBill() {
-        double additionalFee = 0.0;
-
-        if (this.line == null)
-            return -1;
-        else if (line.getPlanType() == GOLD) {
+    private void decideRate() {
+        if (line.getPlanType() == GOLD) {
             // PlanType is Gold
             basicMonthlyRate = 49.95;
             ratePerAdditionalLine = 14.50;
+            ratePerExcessMinute = 0.45;
         }
         else {
             // PlanType is Silver
             basicMonthlyRate = 29.95;
             ratePerAdditionalLine = 21.50;
+            ratePerExcessMinute = 0.54;
         }
+    }
 
-//        expectedBill = basicMonthlyRate;    // 기본금 추가
+    public double getBasicBill() {
+        return basicMonthlyRate;
+    }
+
+    public double getExpectedSimpleBill() {
+        double additionalFee = 0.0;
+
         if (line.getCount() >= 1 && line.getCount() < 4) {
             // if count is 2..3
             additionalFee += (line.getCount()-1) * ratePerAdditionalLine;    // 라인별 요금 추가
@@ -40,7 +46,7 @@ public class Billing {
             additionalFee += 2*ratePerAdditionalLine;
         }
 
-        return basicMonthlyRate + additionalFee;
+        return additionalFee;
     }
 
     public double getBillIncludingExcessMinutes() {
@@ -48,21 +54,11 @@ public class Billing {
 
 //        if (line.getCount() > 1) return -1;
 
-        if (line.getPlanType() == GOLD) {
-            basicMonthlyRate = 49.95;
-            ratePerExcessMinute = 0.45;
-//            expectedBill = basicMonthlyRate;
-            if (line.getUsedTime() > 1000) {
-                additionalFee += (line.getUsedTime()-1000) * ratePerExcessMinute;
-            }
+        if (line.getPlanType() == GOLD && line.getUsedTime() > 1000) {
+            additionalFee += (line.getUsedTime()-1000) * ratePerExcessMinute;
         }
-        else {
-            basicMonthlyRate = 29.95;
-            ratePerExcessMinute = 0.54;
-//            expectedBill = basicMonthlyRate;
-            if (line.getUsedTime() > 500) {
-                additionalFee += (line.getUsedTime()-500) * ratePerExcessMinute;
-            }
+        else if (line.getPlanType() == SILVER && line.getUsedTime() > 500) {
+            additionalFee += (line.getUsedTime()-500) * ratePerExcessMinute;
         }
 
         return additionalFee;
@@ -78,25 +74,13 @@ public class Billing {
         }
 
         int BeneficiaryCount = line.getCount()-3;   // 가족 할인 수혜 대상 인원
-
-        if (line.getPlanType() == GOLD) {
-            basicMonthlyRate = 49.95;
-            ratePerAdditionalLine = 14.50;
-//            expectedBill = basicMonthlyRate;
-            additionalFee += /*(2*ratePerAdditionalLine)*/ + (BeneficiaryCount*5);    // 라인별 요금 추가
-        }
-        else {
-            basicMonthlyRate = 29.95;
-            ratePerAdditionalLine = 21.50;
-//            expectedBill = basicMonthlyRate;
-            additionalFee += /*(2*ratePerAdditionalLine)*/ + (BeneficiaryCount*5);    // 라인별 요금 추가
-        }
+        additionalFee += /*(2*ratePerAdditionalLine)*/ + (BeneficiaryCount*5);    // 라인별 요금 추가
 
         return additionalFee;
     }
 
     public double getCompleteBill() {
-        expectedBill = getExpectedSimpleBill() + getBillIncludingExcessMinutes() + getBillIncludingFamilyDiscount();
+        expectedBill = basicMonthlyRate + getExpectedSimpleBill() + getBillIncludingExcessMinutes() + getBillIncludingFamilyDiscount();
 
         return expectedBill;
     }
--- src/test/java/billingPack/BillingTest.java
+++ src/test/java/billingPack/BillingTest.java
@@ -14,102 +14,104 @@ public class BillingTest {
 
     }
 
-//    @Test
-//    public void expectedSimpleBill_gold_2line() {
-//        billing = new Billing(new Line("seungwon", GOLD, 2, 1000));
-//        Assert.assertEquals(64.45, billing.getExpectedSimpleBill(), 2);
-//    }
-//
-//    @Test
-//    public void expectedSimpleBill_gold_1line() {
-//        billing = new Billing(new Line("seungwon", GOLD, 1, 1000));
-//        Assert.assertEquals(49.95, billing.getExpectedSimpleBill(), 2);
-//    }
-//
-//    @Test
-//    public void expectedSimpleBill_silver_1line() {
-//        billing = new Billing(new Line("seungwon", SILVER, 1, 1000));
-//        Assert.assertEquals(29.95, billing.getExpectedSimpleBill(), 2);
-//    }
-//
-//    @Test
-//    public void expectedSimpleBill_silver_3line() {
-//        billing = new Billing(new Line("seungwon", SILVER, 3, 1000));
-//        Assert.assertEquals(72.95, billing.getExpectedSimpleBill(), 2);
-//    }
-//
-//
-//    @Test
-//    public void calculateExcessMinutes_gold_999m() {
-//        billing = new Billing(new Line("seungwon", GOLD, 1, 999));
-//        Assert.assertEquals(49.95, billing.getBillIncludingExcessMinutes(), 2);
-//    }
-//
-//    @Test
-//    public void calculateExcessMinutes_gold_1000m() {
-//        billing = new Billing(new Line("seungwon", GOLD, 1, 1000));
-//        Assert.assertEquals(49.95, billing.getBillIncludingExcessMinutes(), 2);
-//    }
-//
-//    @Test
-//    public void calculateExcessMinutes_gold_1001m() {
-//        billing = new Billing(new Line("seungwon", GOLD, 1, 1001));
-//        Assert.assertEquals(50.40, billing.getBillIncludingExcessMinutes(), 2);
-//    }
-//
-//    @Test
-//    public void calculateExcessMinutes_gold_1010m() {
-//        billing = new Billing(new Line("seungwon", GOLD, 1, 1010));
-//        Assert.assertEquals(54.45, billing.getBillIncludingExcessMinutes(), 2);
-//    }
-//
-//    @Test
-//    public void calculateExcessMinutes_silver_499m() {
-//        billing = new Billing(new Line("seungwon", SILVER, 1, 499));
-//        Assert.assertEquals(29.95, billing.getBillIncludingExcessMinutes(), 2);
-//    }
-//
-//    @Test
-//    public void calculateExcessMinutes_silver_500m() {
-//        billing = new Billing(new Line("seungwon", SILVER, 1, 500));
-//        Assert.assertEquals(29.95, billing.getBillIncludingExcessMinutes(), 2);
-//    }
-//
-//    @Test
-//    public void calculateExcessMinutes_silver_520m() {
-//        billing = new Billing(new Line("seungwon", SILVER, 1, 520));
-//        Assert.assertEquals(40.75, billing.getBillIncludingExcessMinutes(), 2);
-//    }
-//
-//    @Test
-//    public void calculateFamilyDiscount_gold_2line() {
-//        billing = new Billing(new Line("seungwon", GOLD, 2, 999));
-//        Assert.assertEquals(64.45, billing.getBillIncludingFamilyDiscount(), 2);
-//    }
-//
-//    @Test
-//    public void calculateFamilyDiscount_gold_3line() {
-//        billing = new Billing(new Line("seungwon", GOLD, 3, 999));
-//        Assert.assertEquals(78.95, billing.getBillIncludingFamilyDiscount(), 3);
-//    }
-//
-//    @Test
-//    public void calculateFamilyDiscount_gold_4line() {
-//        billing = new Billing(new Line("seungwon", GOLD, 4, 999));
-//        Assert.assertEquals(83.95, billing.getBillIncludingFamilyDiscount(), 2);
-//    }
-//
-//    @Test
-//    public void calculateFamilyDiscount_silver_4line() {
-//        billing = new Billing(new Line("seungwon", SILVER, 4, 499));
-//        Assert.assertEquals(77.95, billing.getBillIncludingFamilyDiscount(), 2);
-//    }
-//
-//    @Test
-//    public void calculateFamilyDiscount_silver_5line() {
-//        billing = new Billing(new Line("seungwon", SILVER, 5, 499));
-//        Assert.assertEquals(82.95, billing.getBillIncludingFamilyDiscount(), 2);
-//    }
+    @Test
+    public void expectedSimpleBill_gold_2line() {
+        billing = new Billing(new Line("seungwon", GOLD, 2, 1000));
+        Assert.assertEquals(64.45, billing.getBasicBill()+billing.getExpectedSimpleBill(), 2);
+    }
+
+    @Test
+    public void expectedSimpleBill_gold_1line() {
+        billing = new Billing(new Line("seungwon", GOLD, 1, 1000));
+        Assert.assertEquals(49.95, billing.getBasicBill()+billing.getExpectedSimpleBill(), 2);
+    }
+
+    @Test
+    public void expectedSimpleBill_silver_1line() {
+        billing = new Billing(new Line("seungwon", SILVER, 1, 1000));
+        Assert.assertEquals(29.95, billing.getBasicBill()+billing.getExpectedSimpleBill(), 2);
+    }
+
+    @Test
+    public void expectedSimpleBill_silver_3line() {
+        billing = new Billing(new Line("seungwon", SILVER, 3, 1000));
+        Assert.assertEquals(72.95, billing.getBasicBill()+billing.getExpectedSimpleBill(), 2);
+    }
+
+
+    @Test
+    public void calculateExcessMinutes_gold_999m() {
+        billing = new Billing(new Line("seungwon", GOLD, 1, 999));
+        Assert.assertEquals(49.95, billing.getBasicBill()+billing.getBillIncludingExcessMinutes(), 2);
+    }
+
+    @Test
+    public void calculateExcessMinutes_gold_1000m() {
+        billing = new Billing(new Line("seungwon", GOLD, 1, 1000));
+        Assert.assertEquals(49.95, billing.getBasicBill()+billing.getBillIncludingExcessMinutes(), 2);
+    }
+
+    @Test
+    public void calculateExcessMinutes_gold_1001m() {
+        billing = new Billing(new Line("seungwon", GOLD, 1, 1001));
+        Assert.assertEquals(50.40, billing.getBasicBill()+billing.getBillIncludingExcessMinutes(), 2);
+    }
+
+    @Test
+    public void calculateExcessMinutes_gold_1010m() {
+        billing = new Billing(new Line("seungwon", GOLD, 1, 1010));
+        Assert.assertEquals(54.45, billing.getBasicBill()+billing.getBillIncludingExcessMinutes(), 2);
+    }
+
+    @Test
+    public void calculateExcessMinutes_silver_499m() {
+        billing = new Billing(new Line("seungwon", SILVER, 1, 499));
+        Assert.assertEquals(29.95, billing.getBasicBill()+billing.getBillIncludingExcessMinutes(), 2);
+    }
+
+    @Test
+    public void calculateExcessMinutes_silver_500m() {
+        billing = new Billing(new Line("seungwon", SILVER, 1, 500));
+        Assert.assertEquals(29.95, billing.getBasicBill()+billing.getBillIncludingExcessMinutes(), 2);
+    }
+
+    @Test
+    public void calculateExcessMinutes_silver_520m() {
+        billing = new Billing(new Line("seungwon", SILVER, 1, 520));
+        Assert.assertEquals(40.75, billing.getBasicBill()+billing.getBillIncludingExcessMinutes(), 2);
+    }
+
+
+    @Test
+    public void calculateFamilyDiscount_gold_2line() {
+        billing = new Billing(new Line("seungwon", GOLD, 2, 999));
+        Assert.assertEquals(64.45, billing.getBasicBill()+billing.getExpectedSimpleBill()+billing.getBillIncludingFamilyDiscount(), 2);
+    }
+
+    @Test
+    public void calculateFamilyDiscount_gold_3line() {
+        billing = new Billing(new Line("seungwon", GOLD, 3, 999));
+        Assert.assertEquals(78.95, billing.getBasicBill()+billing.getExpectedSimpleBill()+billing.getBillIncludingFamilyDiscount(), 3);
+    }
+
+    @Test
+    public void calculateFamilyDiscount_gold_4line() {
+        billing = new Billing(new Line("seungwon", GOLD, 4, 999));
+        Assert.assertEquals(83.95, billing.getBasicBill()+billing.getExpectedSimpleBill()+billing.getBillIncludingFamilyDiscount(), 2);
+    }
+
+    @Test
+    public void calculateFamilyDiscount_silver_4line() {
+        billing = new Billing(new Line("seungwon", SILVER, 4, 499));
+        Assert.assertEquals(77.95, billing.getBasicBill()+billing.getExpectedSimpleBill()+billing.getBillIncludingFamilyDiscount(), 2);
+    }
+
+    @Test
+    public void calculateFamilyDiscount_silver_5line() {
+        billing = new Billing(new Line("seungwon", SILVER, 5, 499));
+        Assert.assertEquals(82.95, billing.getBasicBill()+billing.getExpectedSimpleBill()+billing.getBillIncludingFamilyDiscount(), 2);
+    }
+
 
     @Test
     public void calculateCompleteBill_gold_4line_878m() {
